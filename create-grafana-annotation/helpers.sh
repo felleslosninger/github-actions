189219149bb2e9db001a8f2b2237b4b1e24b43ac
Sindre Johannessen <sindre.johannessen@digdir.no>
2024-02-02 16:37:59 +0100

    Fixes env (#8)
    * Extracts JSON helper function to seperate file
    * Adds Bats tests
    * Adds workflow to run Bats tests

---
+++ create-grafana-annotation/helpers.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+create_json_payload() {
+  local dashboard_id=$1
+  local panel_id=$2
+  local annotation_text=$3
+  local annotation_url=$4
+
+  if [ -n "$annotation_url" ]; then
+    annotation_text="<a href=\"$annotation_url\">$annotation_text</a>"
+  fi
+
+  jq -n \
+    --arg dashboard_id "$dashboard_id" \
+    --arg panel_id "$panel_id" \
+    --arg text "$annotation_text" \
+    '{"dashboardId": $dashboard_id | tonumber, "panelId": $panel_id | tonumber, "text": $text}'
+}
