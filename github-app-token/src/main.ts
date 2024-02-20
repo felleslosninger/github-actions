2a6073c6b131fed626c4f0404ea4b8601ba3d989
Sindre Johannessen <sindre.johannessen@digdir.no>
2024-02-20 14:56:19 +0100

    Initial commit for github-app-token action

---
+++ github-app-token/src/main.ts
@@ -0,0 +1,60 @@
+import * as core from "@actions/core";
+import { App } from "@octokit/app";
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    const appId = core.getInput("app-id");
+    const privateKey = core.getInput("private-key");
+    const owner = core.getInput("owner") || process.env.GITHUB_REPOSITORY_OWNER;
+    const repository =
+      core.getInput("repository") || process.env.GITHUB_REPOSITORY;
+
+    if (!owner) {
+      throw new Error(
+        "Owner was not supplied as an input or environment variable"
+      );
+    }
+
+    if (!repository) {
+      throw new Error(
+        "Repository was not supplied as an input or environment variable"
+      );
+    }
+
+    const app = new App({
+      appId,
+      privateKey
+    });
+
+    const octokit = app.octokit;
+
+    const { data: installation } = await octokit.request(
+      "GET /repos/{owner}/{repository}/installation",
+      {
+        owner,
+        repository
+      }
+    );
+
+    const installationId = installation.id;
+
+    const {
+      data: { token }
+    } = await octokit.request(
+      `POST /app/installations/${installationId}/access_tokens`,
+      {
+        repositories: [repository]
+      }
+    );
+
+    core.setSecret(token);
+    core.setOutput("token", token);
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message);
+  }
+}
